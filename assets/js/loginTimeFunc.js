91c8635fc6e305a4e7820ad25eda3410f7132ace
Krishrat00 <krishna.rathi.1209@gmail.com>
2023-09-11 10:58:36 +0530

    Cart Functionalities added

---
+++ assets/js/loginTimeFunc.js
@@ -0,0 +1,12 @@
+// Function to store the last login timestamp in localStorage
+export function setLastLogin() {
+    const lastLoginTime = new Date().toString();
+    localStorage.setItem('lastLogin', lastLoginTime);
+  }
+  
+  // Function to retrieve the last login timestamp from localStorage
+  export function getLastLogin() {
+    const lastLogin = localStorage.getItem('lastLogin');
+    return lastLogin;
+  }
+  
