afad2e00f8d603cba27d370c76ad8b8e1b630659
ValsalaSahai <142245558+ValsalaSahai@users.noreply.github.com>
2023-09-13 08:59:34 +0530

    Changes in login and products

---
+++ The-Error-List/README.md
@@ -0,0 +1,2 @@
+# WFS_Pun_2_Team4
+Project Topic: Order Processing System
---
+++ The-Error-List/assets/js/employeeDashboard.js
@@ -0,0 +1,46 @@
+// JavaScript code to fetch and display orders and invoices
+const ordersData = [
+    {
+        id: 1,
+        customerName: "Customer A",
+        orderDate: "2023-09-08",
+        orderValue: "$500",
+        customerCity: "City A",
+        status: "Approved"
+    },
+    {
+        id: 2,
+        customerName: "Customer B",
+        orderDate: "2023-09-09",
+        orderValue: "$800",
+        customerCity: "City B",
+        status: "Completed"
+    },
+    // Add more order data as needed
+];
+
+document.getElementById("view-orders").addEventListener("click", function () {
+    const ordersList = document.getElementById("orders");
+    ordersList.innerHTML = "";
+
+    ordersData.forEach((order) => {
+        const listItem = document.createElement("li");
+        listItem.className = "order-item";
+        listItem.innerHTML = `
+            <h3>Order ID: ${order.id}</h3>
+            <p>Customer Name: ${order.customerName}</p>
+            <p>Order Date: ${order.orderDate}</p>
+            <p>Order Value: ${order.orderValue}</p>
+            <p>Customer City: ${order.customerCity}</p>
+            <p>Status: ${order.status}</p>
+        `;
+        ordersList.appendChild(listItem);
+    });
+
+    const invoice = document.getElementById("invoice");
+    invoice.style.display = "block";
+    invoice.innerHTML = `
+        <h2>Invoice</h2>
+        <p>Invoice details go here.</p>
+    `;
+});
